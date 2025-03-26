8618677571efd36edccb745ca82320c2483157dd
Matt Dziuban <mrdziuban@gmail.com>
2025-03-26 12:06:36 -0400

    v0.1.0

--- project/Common.scala
+++ project/Common.scala
@@ -59,7 +59,7 @@ object Common extends AutoPlugin {
 
   override def globalSettings =
     Seq(
-      version      := "0.1.0-SNAPSHOT",
+      version      := "0.1.0",
       organization := "bondlink",
       homepage     := Some(url(s"https://github.com/mblink/$repoName")),
       licenses     := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
