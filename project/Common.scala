e58c4a65b398a14e974d90c10529cd81ffe542b3
Matt Dziuban <mrdziuban@gmail.com>
2025-04-29 14:15:27 -0400

    Set version to 0.3.2-SNAPSHOT.

--- project/Common.scala
+++ project/Common.scala
@@ -59,7 +59,7 @@ object Common extends AutoPlugin {
 
   override def globalSettings =
     Seq(
-      version      := "0.3.1",
+      version      := "0.3.2-SNAPSHOT",
       organization := "bondlink",
       homepage     := Some(url(s"https://github.com/mblink/$repoName")),
       licenses     := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
